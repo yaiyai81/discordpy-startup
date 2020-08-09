7d10d5436417f38b4fb23f935812ef82dc408855
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-08-09 22:30:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,13 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
 @bot.command()
 async def おはよう(ctx):
     await ctx.send('ねむいな')
