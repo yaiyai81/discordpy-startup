9f61185add626b75768c4e76451bc4b75d57d434
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-08-09 22:36:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,11 +8,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.command()
 async def おはよう(ctx):
     await ctx.send('ねむいな')
-
-@bot.command()
-async def おはよう(ctx):
-    await ctx.send('は？　寝ろ')
-    
+  
 @bot.command()
 async def ななぎして(ctx):
     await ctx.send('な～ぎ')
