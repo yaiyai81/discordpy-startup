418eea6dc486e731d098af2ed4f4d826d583ecf4
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-08-09 18:46:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,7 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+
 
 @bot.command()
 async def neko(ctx):
