939c189bd3b8bfe37c31462e2357cdf8a8b04e06
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-08-09 19:13:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,27 +18,3 @@ async def ねこ(ctx):
 
 bot.run(token)
 
-import discord
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-@client.event
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "は？　寝てろ"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
-
-client.run("token")
-
