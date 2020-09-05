1d9ec9f986b9dba1933325fba42cb0628ff4be72
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-06 01:35:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -40,10 +40,6 @@ async def reply(message):
     reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
 
-# 発言時に実行されるイベントハンドラを定義
-@bot.event
-async def on_message(message):
-    if bot.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
+
         
 bot.run(token)
