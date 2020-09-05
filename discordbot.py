f98a607f07f9fd32b09e0677e23195384cd2ace0
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 18:21:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,6 +14,45 @@ async def on_ready():
     activity = discord.Game(name="アンジニティ", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("Bot is ready!")
+      global result, judge
+    if message.author.bot:  # ボットからのメッセージを回避します
+        return
+
+    if message.content == "！じゃんけん":
+        await message.channel.send("最初はぐー、じゃんけん")
+
+        jkbot = random.choice(("ぐー", "ちょき", "ぱー"))
+        draw = "引き分けだよ～"
+        wn = "君の勝ち！"
+        lst = random.choice(("私の勝ち！弱ｗｗｗｗｗｗｗｗｗｗｗｗやめたら？じゃんけん",
+                              "私の勝ちだね(∩´∀｀)∩、また挑戦してね！"))
+
+        def jankencheck(m):
+            return (m.author == message.author) and (m.content in ['ぐー', 'ちょき', 'ぱー'])
+
+        reply = await client.wait_for("message", check=jankencheck)
+        if reply.content == jkbot:
+            judge = draw
+        else:
+            if reply.content == "ぐー":
+                if jkbot == "ちょき":
+                    judge = wn
+                else:
+                    judge = lst
+
+            elif reply.content == "ちょき":
+                if jkbot == "ぱー":
+                    judge = wn
+                else:
+                    judge = lst
+
+            else:
+                if jkbot == "ぐー":
+                    judge = wn
+                else:
+                    judge = lst
+
+        await message.channel.send(judge)
     
 @bot.command()
 async def おはよう(ctx):
@@ -21,7 +60,7 @@ async def おはよう(ctx):
   
 @bot.command()
 async def ななぎして(ctx):
-    await ctx.send('な～ぎ','ななぎ')
+    await ctx.send('な～ぎ')
 
  
     
