f686082da933e93f573cc0a944936f19232de005
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 18:29:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,9 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
+import random
 
+client = discord.Client()
 bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
 
