6171c5a3fea867b69bd7e03488f0c968e3abbf20
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 23:14:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,19 +16,18 @@ async def on_ready():
     activity = discord.Game(name="アンジニティ", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("Bot is ready!")
-       
+   
 async def on_message(message):
     if "おはよう" in message.content:
         word_list = ["ねむい","なに？まだねてろよ","うるせー起こすな！","https://dl.dropboxusercontent.com/s/3qux2f89rgq1d22/gr120.png"]
         await message.channel.send(random.choice(word_list))
 
-@bot.event
 async def on_message(message):
     if "おやすみ" in message.content:
         word_list = ["はよねな","さっさとねろ","ねみ…"]
         await message.channel.send(random.choice(word_list))
 
-@bot.event
+
 async def on_message(message):
     if "ななぎして" in message.content:
         word_list = ["ななぎして","な～ぎ","なぎさ太った？"]
