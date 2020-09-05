04e2e2b97165fc90be37a89863caf74eb41d3bee
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 21:22:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,11 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("Bot is ready!")
 
+@bot.event
+async def on_message(message):
+    if "!じゃんけん" in message.content:
+        word_list = ["グー","チョキ","パー"]
+        await message.channel.send(random.choice(word_list))
+
     
 bot.run("TOKEN")
