be6052413a067ae7e7f67a0564815548d91be886
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 21:25:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,10 @@
-from discord.ext import commands
-import os
-import traceback
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
+from discord.ext import commands
+import os
+import traceback
 import random
-import logging
 
 bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -19,7 +17,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if "!じゃんけん" in message.content:
+    if "じゃんけん" in message.content:
         word_list = ["グー","チョキ","パー"]
         await message.channel.send(random.choice(word_list))
 
