bf78f6a0190407868b55c5e868eeed12fd2a5dd3
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 22:51:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,7 +20,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if "おはよう" in message.content:
-        word_list = ["ねむい","なに？まだねてろよ",":sinobu:"]
+        word_list = ["ねむい","なに？まだねてろよ","https://dl.dropboxusercontent.com/s/3qux2f89rgq1d22/gr120.png"]
         await message.channel.send(random.choice(word_list))
 
 @bot.event
