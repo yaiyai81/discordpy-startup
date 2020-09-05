b36ceb0a949cf2e928605c99eb98ad473efd4706
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-05 22:11:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,7 @@ import random
 import logging
 import asyncio
 
+client = discord.Client()
 bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -17,13 +18,14 @@ async def on_ready():
     print("Bot is ready!")
     
 @bot.event
-async def on_message(message):
-    if client.user != message.author:
-        if message.content.startswith("おはよう"):
-            m = "おはようございます" + message.author.name + "さん！"
-            await client.send_message(message.channel, m)
-        if message.content.startswith("こんにちは"):
-            m = "こんにちは" + message.author.name + "さん！"
-            await client.send_message(message.channel, m)
+async def on_message(message): #メッセージを受け取る関数なので必ず必要
+    if message.content == "おはよう": #:を忘れずつけよう！Enterを押すと自動で4文字分あけて改行されるよ！
+        await client.send_message(message.channel, "ねむい")
+
+    if message.content == "占い":
+#レスポンスされる運勢のリストを作成
+        unsei = ["大吉", "中吉", "吉", "末吉", "小吉", "凶", "大凶"]
+        choice = random.choice(unsei) #randomモジュール使用
+        await message.send_message(message.channel, choice)
             
 bot.run(token)
