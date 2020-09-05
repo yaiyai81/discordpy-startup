6d7154c45c4d4ccbbba2c2ac303109fdf30355ec
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-06 00:40:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -34,4 +34,12 @@ async def on_message(message):
         word_list = ["ななぎして","な～ぎ","なぎさ太った？",":cat: :boom:"]
         await message.channel.send(random.choice(word_list))
         
+@bot.event
+async def reply(message):
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+    
+    if client.user in message.mentions: # 話しかけられたかの判定
+       await reply(message) # 返信する非同期関数を実行
+    
 bot.run(token)
