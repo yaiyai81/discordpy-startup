66168dd7cc3185def94538382ecae4efa712fc8c
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-07 22:56:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -48,7 +48,7 @@ async def on_message(message):
         
     if message.content == "ネコチャンバトル":
         await message.channel.send("へえ。僕に勝負を挑むとは良い度胸だな……お前はどのねこにする？")
-        embed=discord.Embed(title="火のねこ", description="流るる水のネコチャン。風に強く、水に弱い", color=0xff0000)
+        embed=discord.Embed(title="火のねこ", description="燃え盛る火のネコチャン。風に強く、水に弱い", color=0xff0000)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/740524923847573555/752435155888636015/hinoneko.png")
         await message.channel.send(embed=embed)
         embed=discord.Embed(title="風のねこ", description="吹きすさぶ風のネコチャン。水に強く、火に弱い", color=0x00ff11)
@@ -60,9 +60,9 @@ async def on_message(message):
         
         jkbot = random.choice(("火のねこ", "風のねこ", "水のねこ"))
         draw = random.choice(("あいこじゃん。つまんねーな","あいここ～。もう終わり？"))
-            embed=discord.Embed(title="火のねこ", description="流るる水のネコチャン。風に強く、水に弱い", color=0xff0000)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/740524923847573555/752435155888636015/hinoneko.png")
-            await message.channel.send(embed=embed)
+          embed=discord.Embed(title="火のねこ", description="流るる水のネコチャン。風に強く、水に弱い", color=0xff0000)
+          embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/740524923847573555/752435155888636015/hinoneko.png")
+          await message.channel.send(embed=embed)
             
         draw2 = random.choice(("あいこじゃん。つまんねーな","お前、やる気あんのか？",
                               "はいはい、引き分け","あいこ～。もう終わり？"))
