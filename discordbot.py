b8316083418115d7887c4c6266050017e6b5ee84
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-07 18:42:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,10 @@ async def on_ready():
     activity = discord.Game(name="アンジニティ", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("Bot is ready!")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------') 
     
 @bot.event
 async def on_message(message):
