379f3a65455da71c37699b250fdc695c7b196104
yaiyai81 <69415788+yaiyai81@users.noreply.github.com>
2020-09-08 01:54:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,7 +38,7 @@ async def on_message(message):
         await message.channel.send(random.choice(word_list3))
    
     if bot.user in message.mentions:
-        reply = f'{message.author.mention} ……なんか用？　大した事やんねえからな。\n```[Hello shinobu bot!]\nおはよう、おやすみ、ななぎして\nネコチャンバトル→火風水の三種のねこを使って勝負。```'
+        reply = f'{message.author.mention} ……なんか用？　大した事やんねえからな。\n```[Hello Sinobu bot!]\nおはよう、おやすみ、ななぎして\nネコチャンバトル→火風水の三種のねこを使って勝負。```'
         await message.channel.send(reply)
         
 
